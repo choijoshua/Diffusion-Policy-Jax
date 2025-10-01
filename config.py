6589ee22a14488afeda5699847ffd412dffc3d7f
choijoshua <choijoshua16@gmail.com>
2025-09-30 20:57:20 -0400

    setup config files and hydra hierarchy system for loading algorithms to use

--- config.py
+++ config.py
@@ -1,12 +1,53 @@
 from dataclasses import dataclass
+from omegaconf import MISSING
+from hydra.core.config_store import ConfigStore
+from typing import Optional
 
 @dataclass
-class Args:
-    seed: int = 1
-    dataset: str = "mujoco/halfcheetah/expert-v0"
-    embed_dim: int = 100
-    num_updates: int = 8
-    num_residual_blocks: int = 4
-    num_mid_blocks: int = 2
-    dims: tuple = (32, 64, 128, 256)
+class TrainArgs:
+    # --- Experiment ---
+    algorithm: str = MISSING
+    lr: float = 3e-4
+    num_updates: int = 100000
+    
+    num_timesteps: int = 50
+    
 
+@dataclass
+class DDPMArgs(TrainArgs):
+    algorithm = "ddpm"
+
+@dataclass
+class ScoreMatchingArgs(TrainArgs):
+    algorithm = "score_matching"
+    sigma: float = 3.0
+
+
+@dataclass
+class GeneralArgs:
+    algorithms: TrainArgs = MISSING
+    # --- Experiment ---
+    seed: int = 0
+    dataset: str = MISSING
+    custom_dataset: Optional[str] = None
+    # --- Logging ---
+    log: bool = False
+    save_ckpt: bool = False
+    wandb_project: str = "unifloral"
+    wandb_team: str = "flair"
+    wandb_group: str = "debug"
+    wandb_jobtype: str = "Train"
+
+def register_train_configs() -> None:
+    cs = ConfigStore.instance()
+    
+    cs.store(
+        group="algorithms",
+        name="ddpm",
+        node=DDPMArgs,
+    )
+    cs.store(
+        group="algorithms",
+        name="score_matching",
+        node=ScoreMatchingArgs,
+    )
